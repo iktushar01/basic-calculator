a35d78557024153785e69ddfd3241e9f377bb06d
tushar <iktushar2020@gmail.com>
2025-03-18 22:19:38 +0600

    basic calculator done

--- tailwind.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-    content: ["./*.html"], // Adjust this to your project structure
-    theme: {
-      extend: {},
-    },
-    plugins: [],
-  };
-  
